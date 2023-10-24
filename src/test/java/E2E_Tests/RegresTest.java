b06788eb3c18f544ed5fa7de7dcc63cfbf3f25ac
golenev <golenev.vp@sibvaleo.com>
2023-10-24 16:01:12 +0300

    долив

--- src/test/java/E2E_Tests/RegresTest.java
+++ src/test/java/E2E_Tests/RegresTest.java
@@ -1,4 +1,16 @@
-package regres;
+package E2E_Tests;
 
-public class E2E_Tests {
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Test;
+
+public class RegresTest {
+    @Step("Проверка шаг 1 qweqweqwe")
+    public void someLogic(){
+        System.out.println("консоль");
+    }
+
+    @Test
+    void test(){
+        someLogic();
+    }
 }
